ff7293b936049bef2605e1fd193ece4c6d80017d
Nytezerak <chrysdragon.97@hotmail.com>
2021-08-25 15:25:10 -0300

    Config express server

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log('Feijão')
+const express = require('express');
+const {} = require('path');
+const app = express();
+
+app.use('/',
+express.static(
+    './build'
+    )
+)
+
+app
+.listen(process.env.PORT || 3000, (err) =>{
+    if (err){
+        return console.log(err)
+    }
+    console.log('Tudo ok!')
+});
